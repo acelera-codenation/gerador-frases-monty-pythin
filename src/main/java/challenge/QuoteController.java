4aa3416b4ec22b382686d0f20ee3cc093fab36f6
Samuel Santos <>
2020-07-20 18:30:30 -0300

    Add ajustes

--- src/main/java/challenge/QuoteController.java
+++ src/main/java/challenge/QuoteController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/quote")
+@RequestMapping("v1/quote")
 public class QuoteController {
 
     private QuoteService service;
