0bccb13a911fb02e8e1b681dd7ca0af9d0ebf44f
Samuel Santos <>
2020-07-21 09:52:31 -0300

    Dependency injection

--- src/main/java/challenge/QuoteController.java
+++ src/main/java/challenge/QuoteController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/quote")
 public class QuoteController {
 
-    @Autowired
     private QuoteService service;
+
+    public QuoteController(QuoteService service) {
+        this.service = service;
+    }
 
     @GetMapping
     public Quote getQuote() {
--- src/main/java/challenge/QuoteServiceImpl.java
+++ src/main/java/challenge/QuoteServiceImpl.java
@@ -1,13 +1,15 @@
 package challenge;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuoteServiceImpl implements QuoteService {
 
-    @Autowired
     private QuoteRepository repository;
+
+    public QuoteServiceImpl(QuoteRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public Quote getQuote() {
