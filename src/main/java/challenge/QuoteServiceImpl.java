07225e5e42cff7a8215d39ccb065581a845e8661
Samuel Santos <>
2020-07-21 09:43:47 -0300

    Alterar a forma de pegar um item randomicamente

--- src/main/java/challenge/QuoteServiceImpl.java
+++ src/main/java/challenge/QuoteServiceImpl.java
@@ -2,10 +2,6 @@ package challenge;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Random;
 
 @Service
 public class QuoteServiceImpl implements QuoteService {
@@ -13,21 +9,13 @@ public class QuoteServiceImpl implements QuoteService {
     @Autowired
     private QuoteRepository repository;
 
-    private final Random random = new Random();
-
-    // https://www.baeldung.com/java-random-list-element
-    private Quote getRandom(List<Quote> quotes) {
-        return quotes.isEmpty() ? null : quotes.get(random.nextInt(quotes.size()));
-    }
-
     @Override
     public Quote getQuote() {
-        return getRandom(this.repository.findAll());
+        return repository.findAll().stream().findAny().orElse(null);
     }
 
     @Override
     public Quote getQuoteByActor(String actor) {
-        return getRandom(repository.findByActor(actor));
+        return repository.findByActor(actor).stream().findAny().orElse(null);
     }
-
 }
